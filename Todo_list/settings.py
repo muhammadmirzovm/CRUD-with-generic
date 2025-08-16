bd94286f9df61afd2700287c64dd76157ad03724
muhammadmirzo <mukhammadmirzo078@gmail.com>
2025-08-16 23:53:14 +0500

    Added swagger UI to my DRF  ToDo

--- Todo_list/settings.py
+++ Todo_list/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'todos',
+    'drf_yasg',
     'rest_framework',
 ]
 
@@ -125,7 +126,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+BASE_DIR = Path(__file__).resolve().parent.parent
 
+
+
+# Add this:
+STATIC_ROOT = BASE_DIR / "staticfiles"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
--- Todo_list/urls.py
+++ Todo_list/urls.py
@@ -17,7 +17,40 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from drf_yasg import openapi  as drf_yasg_openapi
+from drf_yasg import views as drf_yasg_views
+from rest_framework import permissions
+from rest_framework.schemas import get_schema_view
+
+
+schema_view = drf_yasg_views.get_schema_view(
+    drf_yasg_openapi.Info(
+        title='ToDo API',
+        default_version = "v1",
+        descrtiption = "ToDo API",
+        contact = drf_yasg_openapi.Contact(email='info.mukhammadmirzo078@gmail.com'),
+        license=drf_yasg_openapi.License(name="Proprietary software license")
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('todos.urls')),
+    
+    #Swagger 
+    # Swagger
+path("schema/", get_schema_view(
+    title="API's",
+    description="API for Erp contract",
+), name="openapi-schema"),
+
+path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+path("swagger/json/", schema_view.without_ui(cache_timeout=0), name="schema-json"),
+path("swagger/yaml/", schema_view.without_ui(cache_timeout=0), name="schema-yaml"),
+
 ]
